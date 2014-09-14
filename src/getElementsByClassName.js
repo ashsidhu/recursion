acb0e1cbfdd3a2868fd8946e303853f750239e21
ashsidhu <ashmeet.sidhu7@gmail.com>
2014-09-14 14:25:21 +0530

    cleanup getElementsByClassName to use conventions in problem statement

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,34 +6,36 @@
 // But instead we're going to implement it from scratch:
 
 // Question: Are we allowed to add custom parameters to definitions?
-var getElementsByClassName = function(className, rootNode){
+var getElementsByClassName = function(className, rootElement){
   var results = [];
-  var rootNode = rootNode || document;
-  if (nodeHasClass(rootNode, className)){
-    results.push(rootNode);
+  var rootElement = rootElement || document.body;
+  if (elementHasClass(rootElement, className)){
+    results.push(rootElement);
   }
 
-  var childNodes = rootNode.childNodes;
-  for (var i = 0; i < childNodes.length; i ++){
-    var nodesWithClass = getElementsByClassName(className, childNodes[i]);
-    if (nodesWithClass.length > 0){
-      results.push(nodesWithClass);
+  var childElements = rootElement.childNodes;
+  for (var i = 0; i < childElements.length; i ++){
+    var elementsWithClass = getElementsByClassName(className, childElements[i]);
+    if (elementsWithClass.length > 0){
+      results.push(elementsWithClass);
     }
   }
 
   // If life was easy, we could just do things the easy way:
   // return _.flatten(results);
+  // But instead we're going to implement it from scratch:
   return flatten(results);
 };
 
-var nodeHasClass = function(node, className){
-  if (node.nodeType === 1){
-    return node.className.indexOf(className) > -1;
+var elementHasClass = function(element, className){
+  if (element.nodeType === 1){
+    return element.classList.contains(className);
   } else {
     return false;
   }
 };
 
+// another recursive function
 var flatten = function(array){
   var result = [];
   for (var i = 0; i < array.length; i++) {
